b1926234b6b05e9be3ca8adce0f38f65bc56144d
uka yura <ukayura@ukas-MacBook-Air.local>
2023-01-19 21:36:24 +0800

    reset btn added

--- src/components/StudentProfile.jsx
+++ src/components/StudentProfile.jsx
@@ -3,6 +3,7 @@ import "../style/StudentPro.css";
 import { userServices } from "../util/service";
 export default function StudentProfile(prop) {
   const { data } = prop;
+  const { hover } = prop;
   useEffect(() => {
     if (data?.status === "active") {
       setChColor("rgb(9, 229, 9)");
@@ -15,8 +16,21 @@ export default function StudentProfile(prop) {
 
   const [chColor, setChColor] = useState("white");
 
+  const handleMouseOver = () => {
+    hover(data.name, true, chColor);
+  };
+
+  const handleMouseOut = () => {
+    hover("reset", false, "white");
+  };
+
   return (
-    <div className="profile" style={{ backgroundColor: chColor }}>
+    <div
+      className="profile"
+      style={{ backgroundColor: chColor }}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
       <img
         src="https://cuad.ask.fm/assets2/064/547/741/696/normal/966259_10200598094587862_251011299_o.jpg"
         alt=""
--- src/page/Teacher.jsx
+++ src/page/Teacher.jsx
@@ -4,7 +4,9 @@ import "../style/Teacher.css";
 import { userServices } from "../util/service";
 export default function Teacher() {
   const [data, setData] = useState();
-
+  const [isHovering, setIsHovering] = useState(false);
+  const [chColor, setChColor] = useState("white");
+  const [name, setName] = useState("reset");
   useEffect(() => {
     userServices
       .getAllUsers()
@@ -12,39 +14,58 @@ export default function Teacher() {
       .then((lol) => setData(lol.data));
   }, []);
   console.log(data);
-
+  function hover(name, hover, color) {
+    setIsHovering(hover);
+    setName(name);
+    setChColor(color);
+  }
+  function jd() {
+    data.map((user) => {
+      userServices.updateUser(user._id, "none");
+      console.log(user);
+    });
+  }
   return (
     <div className="students">
       {data && (
         <>
           <div className="top">
-            <StudentProfile data={data[9]} />
-            <StudentProfile data={data[8]} />
-            <StudentProfile data={data[7]} />
-            <StudentProfile data={data[6]} />
-            <StudentProfile data={data[5]} />
+            <StudentProfile data={data[9]} hover={hover} />
+            <StudentProfile data={data[8]} hover={hover} />
+            <StudentProfile data={data[7]} hover={hover} />
+            <StudentProfile data={data[6]} hover={hover} />
+            <StudentProfile data={data[5]} hover={hover} />
           </div>
           <div className="bottom">
             <div className="left">
-              <StudentProfile data={data[10]} />
-              <StudentProfile data={data[11]} />
-              <StudentProfile data={data[12]} />
-              <StudentProfile data={data[13]} />
-              <StudentProfile data={data[14]} />
+              <StudentProfile data={data[10]} hover={hover} />
+              <StudentProfile data={data[11]} hover={hover} />
+              <StudentProfile data={data[12]} hover={hover} />
+              <StudentProfile data={data[13]} hover={hover} />
+              <StudentProfile data={data[14]} hover={hover} />
             </div>
             <div className="center">
-              <StudentProfile data={data[15]} />
-              <StudentProfile data={data[16]} />
-              <StudentProfile data={data[17]} />
-              <StudentProfile data={data[18]} />
-              <StudentProfile data={data[19]} />
+              <div className="centerPro">
+                <StudentProfile data={data[15]} hover={hover} />
+                <StudentProfile data={data[16]} hover={hover} />
+                <StudentProfile data={data[17]} hover={hover} />
+                <StudentProfile data={data[18]} hover={hover} />
+                <StudentProfile data={data[19]} hover={hover} />
+              </div>
+              <button
+                className="btnReset"
+                style={{ backgroundColor: chColor }}
+                onClick={jd}
+              >
+                {name}
+              </button>
             </div>
             <div className="right">
-              <StudentProfile data={data[4]} />
-              <StudentProfile data={data[3]} />
-              <StudentProfile data={data[2]} />
-              <StudentProfile data={data[1]} />
-              <StudentProfile data={data[0]} />
+              <StudentProfile data={data[4]} hover={hover} />
+              <StudentProfile data={data[3]} hover={hover} />
+              <StudentProfile data={data[2]} hover={hover} />
+              <StudentProfile data={data[1]} hover={hover} />
+              <StudentProfile data={data[0]} hover={hover} />
             </div>
           </div>
         </>
